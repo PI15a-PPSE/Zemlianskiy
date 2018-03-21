bdfa2264d36d6fa616f40c3eec897bf0c27df95b
ZemlianskiyDmitriy <velheor19375@mail.ru>
2018-03-21 22:12:02 +0300

    Create Bricks
    Заполнение игровой площадки кирпичиками

--- Arcanoid.js
+++ Arcanoid.js
@@ -8,6 +8,15 @@ function PlayingArea()
     this.width = this.element.width();
     this.height = this.element.height();
     
+    
+    this.initBricks = function() // функция заполнения площадки блоками
+    {
+        for (var i = 0; i < countBricksLines; i++)
+            for (var j = 0; j < countBricksLine; j++)
+                this.element.prepend("<div id='brick_"+i+"_"+j+"' class='brick' style='left:"+(j * widthBricks + 1)+"px; top:"
+                    +(i * heightBricks + 1)+"; width:"+(widthBricks - 2)+"px; height: "+(heightBricks - 2)+"px'><p>"
+                        + (Math.round(Math.random() * (countBricksLines - i)) + (countBricksLines - i)) + "</p></div>");
+    }
 }
 
 
@@ -22,4 +31,5 @@ var pa; // игровая площадка
 $(document).ready(function() 
 {
     pa = new PlayingArea();
+    pa.initBricks();
 })
